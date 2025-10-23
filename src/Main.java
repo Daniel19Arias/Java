9e87e6411b122e8164e9918896c295c68f8b90cc
unknown <danielsetita29@gmail.com>
2025-10-23 13:07:08 +0200

    Ejercicio de Java 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String []args){
+        System.out.print("Hola ");
+        System.out.println("Clase Programacion");
+        System.out.printf("Son las: %.2f",  10.5032);
+
+        String nombre = "Daniel";
+        String apellido = "Arias";
+        System.out.println("\nTe llamas "+nombre+"      "+ apellido);
+
+    }
+}
