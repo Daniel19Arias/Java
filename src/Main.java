dd78a4c0959996093bf93f74326f08eb48a0a067
unknown <danielsetita29@gmail.com>
2025-10-27 08:41:47 +0100

    Ejercicio Java 4 (Calculadora IMC)

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args){
         //Utilizamos dos escáners, uno para la altura y otro para el peso
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Escribe a continuación la altura en m: ");
+        System.out.print("Escribe a continuación la altura en cm: ");
         float altura = scanner.nextFloat();
         System.out.print("\nEscribe a continuación su peso en KG: ");
         float peso = scanner.nextFloat();
